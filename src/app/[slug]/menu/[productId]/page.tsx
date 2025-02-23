6141ae45c888d690714d661bf72d16c645a998e7
Samuel Petten <samuelrcvp@gmail.com>
2025-02-23 16:08:44 -0300

    feat: add product details page

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
+import ProductDetails from "./components/product-details";
 import ProductHeader from "./components/product-header";
 
 interface ProductPageProps{
@@ -12,17 +13,30 @@ interface ProductPageProps{
 
 const ProductPage = async ({params}: ProductPageProps) => {
     const {slug, productId} = await params;
-    const product = await db.product.findUnique({where:{id:productId}});
+    
+    const product = await db.product.findUnique({
+        where:{id:productId}, 
+        include:{
+        restaurant:{
+            select:{
+                slug:true,
+                name: true,
+                avatarImageUrl: true,
+            }
+        },
+    }
+});
     if (!product) {
         return notFound();
     }
+    if (product.restaurant.slug.toLocaleUpperCase() != slug.toUpperCase()) {
+        return notFound();
+    }
     return ( 
-        <>
-        <ProductHeader product={product}/>
-        <h1>Product Page</h1>
-        {slug}
-        {productId}
-        </>
+        <div className="flex h-full flex-col">
+            <ProductHeader product={product}/>
+            <ProductDetails product={product}/>
+        </div>
      );
 }
  
